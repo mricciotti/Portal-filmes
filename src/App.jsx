e074ac3b78e5a99c90625320c7afd6d2d8a8fc5e
Matheus Ricciotti <matheus.ricciotti@gmail.com>
2024-09-13 22:40:37 -0300

    fazendo o header e a main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-function App() {
+// function App() {
 
-  return (
+//   return (
+//     <>
+//      <h1>Home Page</h1>
+//      {/* Exibe o header e a lista de filmes disponível divididos por
+//      Antigos(antes dos anos 2000)
+//      Bem-avaliados(nota maior que 9)*/}
+//     </>
+//   )
+// }
+
+// export default App
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+
+
+function App(){
+
+  return(
     <>
-     <h1>Home Page</h1>
-     {/* Exibe o header e a lista de filmes disponível divididos por
-     Antigos(antes dos anos 2000)
-     Bem-avaliados(nota maior que 9)*/}
+    <Header />
+    <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import MovieListPage from './pages/MovieListPage.jsx'
+import MoviesByGenrePage from './pages/MoviesByGenrePage.jsx'
+import GenreListPage from './pages/GenreListPage.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
 
-{/* Deve ter as rotas:
-  home
-  filmes
-  Detalhes do Filme
-  Lista de gêneros
-  Filmes por gênero
-  Page Not Found
-  */}
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children: [
+        {index: true, element: <Home/>},
+        {path: "filmes", element: <MovieListPage/>},
+        {path: "filmes/:id", element: <MovieDetailPage/>},
+        {path: "genero", element: <GenreListPage/>},
+        {path: "genero/:id", element: <MoviesByGenrePage/>},
+        {path: "*", element: <PageNotFound/>},
+      ]
+    }
+  ])
+  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
