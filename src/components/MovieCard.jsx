3479b8f0d3d955261fe257a0a3919fa79ba423d5
Matheus Ricciotti <matheus.ricciotti@gmail.com>
2024-11-01 20:19:49 -0300

    arrumando o MovieCard, MovieDetailPage Home

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function MovieCard({id , titulo, imagem_destaque}) {
+export default function MovieCard({id , titulo, imagem_destaque, poster_path, backdrop_path}) {
     return(
         <div > 
             <h2>{titulo}</h2>
             <img src={imagem_destaque} />
+            <img src={`https://image.tmdb.org/t/p/w154${poster_path}`} />
+            <img src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`} />
+
             <Link to={`/filmes/${id}`} className="">Ver detalhes </Link>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,43 @@
-import CardContainer from "../components/CardContainer"
-import MovieCard from "../components/MovieCard"
+import { useEffect, useState } from "react";
+import CardContainer from "../components/CardContainer";
+import MovieCard from "../components/MovieCard";
 import movies from "../data/movies.json"
 
-export default function Home(){
 
+export default function Home() {
+
+    const [filmesPopulares, setFilmesPopulares] = useState([])
+    const [filmesTrending, setFilmesTrending] = useState([])
+    const [filmesUpcoming, setFilmesUpcoming] = useState([])
+
+    const fetchMovies = async () => {
+        try {
+            //Juntando todos os fetches
+            const [respostaPopulares, respostaTrending, respostaUpcoming] = await Promise.all(
+                [
+                    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=pt-br`),
+                    fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${import.meta.env.VITE_API_KEY}&language=pt-br`),
+                    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_API_KEY}&language=pt-br`)
+                ]
+            )
+
+            //Convertendo em JSON
+            const popularData = await respostaPopulares.json()
+            const trendingData = await respostaTrending.json()
+            const upcomingData = await respostaUpcoming.json()
+
+            //Atualizar o estado
+            setFilmesPopulares(popularData.results)
+            setFilmesTrending(trendingData.results)
+            setFilmesUpcoming(upcomingData.results)
+
+        }
+        catch { }
+
+    }
+    useEffect(() => {
+        fetchMovies();
+    }, [])
     return(
         <>
             <CardContainer titulo="Filmes Antigos">
@@ -26,4 +60,19 @@ export default function Home(){
             </CardContainer>
         </>
     )
+
+    // return (
+    //     <>
+    //         <CardContainer titulo="Populares">
+    //              {
+    //                  movies
+    //                  .filter( filme => (filme.avaliacao > 9))
+    //                  .map( filme => (
+    //                      <MovieCard key={filme.id} {...filme}/>
+    //                  ))
+    //              }
+    //         </CardContainer>
+
+    //     </>
+    // )
 }
